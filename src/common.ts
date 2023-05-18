cbf0aefa0789fe99c23b9be14140493b431fef44
Stephen <stepan.kopylov@ibitcy.com>
2023-05-18 03:26:27 +0300

    wip

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,7 @@
+export enum Templates {
+    Email,
+}
+
 interface IQueryParams {
     lang?: string;
     template?: string;
@@ -6,5 +10,15 @@ interface IQueryParams {
 export const queryCheck = (query: IQueryParams) => {
     if (!query.template) {
         throw new Error('template is required')
+    } else {
+        let found = false;
+        Object.values(Templates).forEach((value) => {
+            if (value.toString().toLowerCase() === query.template.toLowerCase()) {
+                found = true;
+            }
+        });
+        if (!found) {
+            throw new Error("can't find template")
+        }
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,43 +3,37 @@ import express from 'express';
 import * as React from 'react';
 import i18n from "./locale/i18n";
 import {Email} from "./emails";
-import {queryCheck} from "./common";
+import {queryCheck, Templates} from "./common";
 import livereload from "livereload";
 import connectLiveReload from "connect-livereload";
-
-
 
 const liveReloadServer = livereload.createServer();
 liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
         liveReloadServer.refresh("/");
-    }, 50);
+    }, 100);
 });
 
 let app = express();
 
 app.use(connectLiveReload());
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     const query = req.query;
     queryCheck(query);
     if (query.lang && typeof query.lang === 'string') {
         i18n.changeLanguage(query.lang);
     }
-    const template = query.template;
-    const emailHtml = render(<Email url="https://example.com"/>);
-    res.send(emailHtml);
+    const template = Templates[query.template as keyof typeof Templates];
+
+    switch (template) {
+        case Templates.Email: {
+            const emailHtml = render(<Email url={query.url}/>);
+            res.send(emailHtml);
+        }
+    }
 })
 
 let server = app.listen(8081, function () {
     console.log("Example app listening at http://%s:%s", server.address(), server.address())
 })
-
-
-// var argv = require('minimist')(process.argv.slice(2));
-//
-// i18n.changeLanguage(argv.lang);
-//
-// const emailHtml = render(<Email url="https://example.com"/>);
-//
-// console.log(emailHtml);
